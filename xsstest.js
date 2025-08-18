864b0950488550d474c8f635f587100c2f3b8815
nakamura-yrk <nakamura_yrk@msoft.co.jp>
2025-08-18 14:45:32 +0900

    Update xsstest.js

--- xsstest.js
+++ xsstest.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/search', (req, res) => {
   const userInput = req.query.q;
 
-  // 【脆弱性】ユーザーからの入力を無害化せずに、HTMLに直接埋め込んでいる
+  // 【脆弱性】ユーザーからの入力を無害化せずに、HTMLに直接埋め込み
   res.send(`<h1>検索結果: ${userInput}</h1>`);
 });
 
